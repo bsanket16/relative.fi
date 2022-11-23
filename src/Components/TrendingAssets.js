73e5b0140a967b8742bc84a37883b696975e3b8d
bsanket16 <bsankt16@gmail.com>
2022-11-23 20:50:12 +0530

    pt.2 code clean up

--- src/Components/TrendingAssets.js
+++ src/Components/TrendingAssets.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import PropTypes from 'prop-types';
 import {AssetCard} from './AssetCard'
 
 export const TrendingAssets = (props) => {
+  const [coinData, setCoinData] = useState([])
+
+  useEffect(() => {
+    axios.get('https://trending-assets-api.onrender.com/trendingAssets')
+    .then((response) => {
+      setCoinData(response.data)
+    })
+  }, [])
+
   return (
     <div className='container bg-navyDark font-tomorrow pl-4 m-auto'>
       
@@ -12,8 +22,41 @@ export const TrendingAssets = (props) => {
       </div>
 
       <div className='text-whiteLight flex mt-[30px] select-none'>
-        {props.assets.map((coin) => {
-            return <AssetCard assets = {coin} key={coin.id}/>
+
+        {coinData.map((coin) => {
+
+          let {slug, tvl} = coin
+          let slugIcon = '', pairs = []
+
+          // getting the icons
+          props.assets.forEach((item) => {
+
+            if (item.slug === slug) {
+              slugIcon = item.img
+            }
+
+            coin.pairedAssetSlugs.forEach((pair) => {
+              if (pair === item.slug) {
+                pairs.push(item.img)
+              }
+            })
+
+          })
+
+          // rendering card
+          // hardcoded missing values
+          return <AssetCard assets = {
+            {
+              icon: slugIcon,
+              title: slug,
+              current: "$31,812.80",
+              return: 100,
+              tvl: tvl,
+              pairs: pairs
+            }
+          }
+        
+          key= {slug}/>
         })}
       </div>
 
