93605be2768ba428e8cf4bd5e63e94797cd21b8e
bsanket16 <bsankt16@gmail.com>
2022-11-28 13:16:44 +0530

    modified coinIcons object

---
+++ src/Contents/coinIcons.js
@@ -0,0 +1,23 @@
+module.exports = {
+  coinIcons : {
+
+    bitcoin: require('../Contents/icons/Bitcoin.png'),
+
+    fantom: require('../Contents/icons/Ethereum.png'),
+    
+    solana: require('../Contents/icons/Solana.png'),
+    
+    chainlink: require('../Contents/icons/Binance Coin.png'),
+    
+    ethereum: require('../Contents/icons/Ethereum.png'),
+    
+    aave: require('../Contents/icons/SHIBA INU.png'),
+    
+    filecoin: require('../Contents/icons/Solana.png'),
+    
+    uniswap: require('../Contents/icons/Bitcoin.png'),
+    
+    arweave: require('../Contents/icons/Binance Coin.png')
+
+  }
+}
